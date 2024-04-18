4d629325b9c80d1d03edd65c673ee5058f905e74
apavlovspb <maxthon812@gmail.com>
2024-04-18 18:48:35 +0400

    Добавить карту проекта (#10)
    * feat(spec-box): add project graph
    
    * fix
    
    ---------
    
    Co-authored-by: Aleksandr Pavlov <avpspb@yandex-team.ru>
    Co-authored-by: Dmitry Andriyanov <dima117a@yandex-team.ru>

--- src/components/ProjectFeatures/ProjectFeatures.cn.ts
+++ src/components/ProjectFeatures/ProjectFeatures.cn.ts
@@ -1,5 +1,11 @@
 import { cn } from '@bem-react/classname';
 
+export const RATE_LIMIT = 20;
+export const COLORS: Record<string, string> = {
+  success: 'green',
+  error: 'red',
+  warn: 'orange',
+};
 export const INDENT = 32;
 
 export const bem = cn('ProjectFeatures');
