ad0294767b55ee7faac12ec467ee50b8f6b2e6ae
Dmitry Andriyanov <dima117a@yandex-team.ru>
2024-05-27 14:09:33 +0300

    разделение на бандлы

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,44 @@
-import { URL, fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+
+const mapChunks = (id: string) => {
+  if (id.includes('node_modules')) {
+    if (id.includes('node_modules/chart.js')) {
+      return 'chartjs';
+    }
+    if (id.includes('node_modules/react-dom')) {
+      return 'react-dom';
+    }
+
+    return 'vendor';
+  }
+
+  return undefined;
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: (id) =>
-          id.includes("node_modules") ? "vendor" : undefined,
+        manualChunks: mapChunks,
       },
     },
   },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  assetsInclude: ["/api/**"],
+  assetsInclude: ['/api/**'],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5059",
+      '/api': {
+        target: 'http://localhost:5059',
         changeOrigin: true,
       },
     },
