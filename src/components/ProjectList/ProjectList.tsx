3f2b8c9d738d2b68399645f2b2aeaa89e6826d5b
Dmitry Andriyanov <dima117a@gmail.com>
2025-05-29 19:43:13 +0300

    добавил пагинацию проектов на главной

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
+import { Pagination, PaginationProps } from '@gravity-ui/uikit';
 
 import { Project } from '@/types';
 
@@ -11,10 +12,37 @@ export interface ProjectListProps {
   projects: Project[];
 }
 
+const PAGE_SIZE = 8;
+
+function paginate<T>(array: T[], page: number, pageSize: number) {
+  // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
+  return array.slice((page - 1) * pageSize, page * pageSize);
+}
+
 export const ProjectList: FC<ProjectListProps> = (props) => {
   const { projects } = props;
+  const [page, setPage] = useState(1);
 
-  const items = projects.map((p) => <Item key={p.code} project={p} />);
+  const handleUpdate: PaginationProps['onUpdate'] = useCallback((page) => setPage(page), []);
 
-  return <div className={bem()}>{items}</div>;
+  const pagination =
+    projects.length <= PAGE_SIZE ? null : (
+      <div className={bem('Pagination')}>
+        <Pagination
+          page={page}
+          pageSize={PAGE_SIZE}
+          total={projects.length}
+          onUpdate={handleUpdate}
+        />
+      </div>
+    );
+
+  const items = paginate(projects, page, PAGE_SIZE).map((p) => <Item key={p.code} project={p} />);
+
+  return (
+    <div className={bem()}>
+      <div className={bem('Items')}>{items}</div>
+      {pagination}
+    </div>
+  );
 };
